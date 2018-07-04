d0e54fb667fd79fa7fbd48d2f4bf8705f35f32ea
Oluseyi Anani <anasey2018@gmail.com>
2018-07-04 19:11:29 +0100

    reverse function using vanilla javascript

---
+++ Javascript/reverse.js
@@ -0,0 +1,20 @@
+/*
+*Author: Anani Oluseyi
+*Description: Write a function that takes in a string as an argument
+*and returns the reverse of the string.
+*
+*/
+//Using vanila Javascript
+function reverseStr(str){
+    //Determines the number of characters in the string
+    var strLength = str.length - 1;
+    var result = "";
+    //Loop through the string and at every iteration, reduce by 1;
+    for(var i = strLength; i >= 0; i--){
+        result += str[i];
+    }
+    return result;
+}
+
+console.log(reverseStr("Lagos is a very cool place to live in"));
+console.log(reverseStr("I love to code"));
